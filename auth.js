3348bef4063e814dba0c3809aaed515f3de5743d
djamesOX <darrell.james@ox.security>
2025-06-16 12:38:24 -0400

    Update auth.js
    Removed the key 
    
    Signed-off-by: djamesOX <darrell.james@ox.security>

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useHistory } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import SplashScreen from "src/components/SplashScreen";
-console.log("ASIAW375RVVG1BLBSLEQ") 
+ 
 const LoginView = () => {
   const { isAuthenticated, loginWithRedirect } = useAuth0();
   const history = useHistory();
